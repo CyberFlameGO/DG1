d1b5de1ff127897c380e9168720a91e1f05a26f2
Rien Bijl <helloitsrien@gmail.com>
2018-09-17 11:30:45 +0200

    Basis koppelen / html structuur
    Basis opgesteld

---
+++ src/assets/css/vorigeopleiding.php
@@ -0,0 +1,37 @@
+<?php require_once './assets/includes/head.php' ?>
+
+<div class="container"> <br> <br>
+  <div class="row">
+    <div class="col-4">
+
+      <div class="imagecard">
+
+        <img src="./assets/img/rsgterapel.jpg" class="responsive" alt="">
+        <div class="ic-title">
+          RSG Ter Apel
+        </div>
+        <div class="ic-content">
+          Dit is een foto van de vooringang van mijn oude Middelbare School.
+        </div>
+
+      </div>
+
+    </div>
+    <div class="col-8">
+
+      <div class="card">
+        Ik heb 4 jaar lang het VMBO-TL opleidingstraject van de RSG Ter Apel gevolgd. Dit heb ik met goed gevolg afgesloten.
+        De school was leuk, alleen interreseerde geen enkel vak mij.
+      </div>
+
+      <br>
+
+      <iframe style="width: 100%; height: 200px;" frameborder="0" style="border:0"
+src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJrxVTyoLBt0cRUo890mPIJV4&key=AIzaSyA45tBsGeOe5sbWY3hLgq8_O4FBJMsBKoU " allowfullscreen></iframe>
+
+
+    </div>
+  </div>
+</div>
+
+<?php require_once './assets/includes/footer.php' ?>
---
+++ src/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>ICT Forum</title>
+
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/custom.css">
+  </head>
+  <body>
+    <div class="header">
+      <div class="container">
+
+        <div class="row">
+
+          <div class="col-sm-4">
+            <h3>ICT Forum</h3>
+            <b>Lorem ipsum</b>
+          </div>
+
+          <div class="col-sm-8">
+            <nav class="navbar navbar-inverse navbar-margin">
+              <div class="container-fluid">
+
+
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
+                  <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Titel</a></li>
+                    <li><a href="#">Titel</a></li>
+                    <li><a href="#">Titel</a></li>
+
+                  </ul>
+                </div>
+              </div>
+            </nav>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+  </body>
+</html>
